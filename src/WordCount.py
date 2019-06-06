abf73331fab9441b9d494b004815927afebea373
Thiago Resende <thiago.gcresende@gmail.com>
2019-06-05 22:18:44 -0300

    map reduce

--- src/WordCount.py
+++ src/WordCount.py
@@ -1,2 +1,11 @@
-#from pyspark import SparkContext, SparkConf
-print('oi!')
+# -*- coding: utf-8 -*-
+from pyspark import SparkContext, SparkConf
+conf = SparkConf().setAppName("WordCount")
+sc = SparkContext.getOrCreate();
+contentRDD = sc.textFile("/user/cloudera/shakespeare.txt")
+filter_empty_lines = contentRDD.filter(lambda x: len(x) > 0)
+words = filter_empty_lines.flatMap(lambda x: x.split(' '))
+wordcount = words.map(lambda x:(x,1)) \
+    .reduceByKey(lambda x, y: x + y) \
+    .map(lambda x: (x[1], x[0])).sortByKey(False)
+wordcount.saveAsTextFile("/user/cloudera/shakespeare_clean")
